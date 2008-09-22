cf83770b4e35a427962383f461797ff772bb00fb
fishtrees <fishtrees@02afeb50-87f6-11dd-aace-ddc5e87cd236>
2008-09-22 18:01:16 +0000

    add path of models,ctrls,views,configs to Fynd_Application class,so,other object which need find models,views.ctrls,configs can find these resource or class.

--- Fynd/src/Application.php
+++ Fynd/src/Application.php
@@ -1,29 +1,74 @@
 <?php
-include_once 'RequestHandler.php';
-final class Fynd_Application
+require_once 'PublicPropertyClass.php';
+require_once 'RequestHandler.php';
+class Fynd_Application extends Fynd_PublicPropertyClass
 {
-	private static $_instance = null;
-	/**
-	 *
-	 * @return Fynd_Application
-	 */
-	public static function GetInstance()
-	{
-		if(!self::$_instance instanceof Fynd_Application)
-			self::$_instance = new Fynd_Application();
-		return self::$_instance;	
-	}
-	
-	public function Run()
-	{
-		$ctrl = Fynd_RequestHandler::GetControllerName();
-		$action = Fynd_RequestHandler::GetControllerAction();
-		$ctrl .= 'Ctrl';
-		$action .= 'Act';
-		
-		include('controllers/'.$ctrl.'.php');
-		$ctrlInstance = new $ctrl();
-		$ctrlInstance->$action();
-	}
+    protected $_ctrlPath = '';
+    protected $_viewPath = '';
+    protected $_modelPath = '';
+    protected $_configPath = '';
+    /**
+     * single instance of Fynd_Application
+     *
+     * @var Fynd_Application
+     */
+    private static $_instance = null;
+    /**
+     * single instance factory
+     * @return Fynd_Application
+     */
+    public static function getInstance ()
+    {
+        if (! self::$_instance instanceof Fynd_Application)
+        {
+            self::$_instance = new Fynd_Application();
+            self::$_instance->init();
+        }
+        return self::$_instance;
+    }
+    public static function getCtrlPath ()
+    {
+        return self::getInstance()->CtrlPath;
+    }
+    public static function getModelPath ()
+    {
+        return self::getInstance()->ModelPath;
+    }
+    public static function getViewPath ()
+    {
+        return self::getInstance()->ViewPath;
+    }
+    public static function getConfigPath ()
+    {
+        return self::getInstance()->ConfigPath;
+    }
+    private function __construct ()
+    {}
+    /**
+     * Get directory which application work in,end with '/'
+     *
+     * @return string
+     */
+    public function getAppWorkPath ()
+    {
+        return realpath('../');
+    }
+    public function init ()
+    {
+        $this->_ctrlPath = $this->getAppWorkPath() . 'app/controllers/';
+        $this->_modelPath = $this->getAppWorkPath() . 'app/models/';
+        $this->_viewPath = $this->getAppWorkPath() . 'app/views/';
+        $this->_configPath = $this->getAppWorkPath() . 'app/configs/';
+    }
+    public function run ()
+    {
+        $ctrl = Fynd_RequestHandler::GetControllerName();
+        $action = Fynd_RequestHandler::GetControllerAction();
+        $ctrl .= 'Ctrl';
+        $action .= 'Act';
+        include ($this->_ctrlPath . $ctrl . '.php');
+        $ctrlInstance = new $ctrl();
+        $ctrlInstance->$action();
+    }
 }
 ?>
