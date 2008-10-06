2db4f6c67d0da2f51b177184b37c2d61a4cedef5
fishtrees <fishtrees@02afeb50-87f6-11dd-aace-ddc5e87cd236>
2008-10-06 16:26:34 +0000

    add comment for each method and property fix pass both GET and POST data to action method of controller

--- Fynd/src/Application.php
+++ Fynd/src/Application.php
@@ -1,11 +1,45 @@
 <?php
 require_once 'PublicPropertyClass.php';
 require_once 'RequestHandler.php';
+/**
+ * This is global object of whole application,
+ * it dispatch request,hold global variables.
+ * it will be only single instance of lifetime.
+ *
+ */
 class Fynd_Application extends Fynd_PublicPropertyClass
 {
+    /**
+     * The path of controllers,
+     * accessed by $CtrlPath property of Fynd_Application instance, or getCtrlPath static method
+     * can be used in include sentence etc.
+     *
+     * @var string
+     */
     protected $_ctrlPath = '';
+    /**
+     * The path of views,
+     * accessed by $ViewPath property of Fynd_Application instance or getViewPath static method
+     * can be used in include sentence etc.
+     *
+     * @var string
+     */    
     protected $_viewPath = '';
+    /**
+     * The path of models,
+     * accessed by $ModelPath property of Fynd_Application instance, or getModelPath static method
+     * can be used in include sentence etc.
+     *
+     * @var string
+     */    
     protected $_modelPath = '';
+    /**
+     * The path of configures,
+     * accessed by $ConfigPath property of Fynd_Application instance,or getConfigPath static method
+     * can be used in include sentence etc.
+     *
+     * @var string
+     */    
     protected $_configPath = '';
     /**
      * single instance of Fynd_Application
@@ -26,18 +60,38 @@ class Fynd_Application extends Fynd_PublicPropertyClass
         }
         return self::$_instance;
     }
+    /**
+     * Get path of application's controllers.
+     *
+     * @return string
+     */
     public static function getCtrlPath ()
     {
         return self::getInstance()->CtrlPath;
     }
+    /**
+     * Get path of application's models.
+     *
+     * @return string
+     */
     public static function getModelPath ()
     {
         return self::getInstance()->ModelPath;
     }
+    /**
+     * Get path of application's views.
+     *
+     * @return string
+     */
     public static function getViewPath ()
     {
         return self::getInstance()->ViewPath;
     }
+    /**
+     * Get path of application's configures.
+     *
+     * @return string
+     */
     public static function getConfigPath ()
     {
         return self::getInstance()->ConfigPath;
@@ -53,6 +107,11 @@ class Fynd_Application extends Fynd_PublicPropertyClass
     {
         return realpath('../').'/';
     }
+    /**
+     * Initialize application's execution environment,
+     * read global configures etc.
+     *
+     */
     public function init ()
     {
         $this->_ctrlPath = $this->getAppWorkPath() . 'app/controllers/';
@@ -60,6 +119,12 @@ class Fynd_Application extends Fynd_PublicPropertyClass
         $this->_viewPath = $this->getAppWorkPath() . 'app/views/';
         $this->_configPath = $this->getAppWorkPath() . 'app/configs/';
     }
+    /**
+     * Go go go!
+     * read request infomation,choose a proccessor to handle this request
+     * and output result.
+     *
+     */
     public function run ()
     {
         $ctrl = Fynd_RequestHandler::GetControllerName();
@@ -68,7 +133,7 @@ class Fynd_Application extends Fynd_PublicPropertyClass
         $action .= 'Act';
         include ($this->_ctrlPath . $ctrl . '.php');
         $ctrlInstance = new $ctrl();
-        $ctrlInstance->$action();
+        $ctrlInstance->$action($_REQUEST);
     }
 }
 ?>
