9edb65ba571266df90134447f6dffc262c287425
fishtrees@gmail.com <fishtrees@gmail.com@02afeb50-87f6-11dd-aace-ddc5e87cd236>
2009-04-28 12:54:39 +0000

    fix now ,YUI Layout and MenuBar can be display correctly

--- chishazi.cn/build/src/pub/res/js/main.js
+++ chishazi.cn/build/src/pub/res/js/main.js
@@ -1,45 +1,182 @@
-﻿var Cn = {};
-Cn.Chishazi = {};
-Cn.Chishazi.Admin =
-{
-    layout: null
+﻿YAHOO.util.Event.throwErrors = true;
+var Cn = {
+    Chishazi : {
+        Admin : {}
+    }
+};
+Cn.Chishazi.Admin = {
+    layout :null,
+    menu :null
 };
 
-(function() {
-    var loader = new YAHOO.util.YUILoader();
-    loader.require = ['menu'];
-    loader.onSuccess = function() {
-        Cn.Chishazi.Admin.layout = new YAHOO.widget.Layout({
-            minWidth: 1000,
-            units: [
-                { position: 'top',
-                    height: 50,
-                    resize: false,
-                    body: 'header'
+( function() {
+    var initTopMenu = function() {
+        /*
+         * Define an array of object literals, each containing the data
+         * necessary to create the items for a MenuBar.
+         */
+
+        var aItemData = [
+
+                {
+                    text :"<em id=\"yahoolabel\">吃啥子</em>",
+                    submenu : {
+                        id :"yahoo",
+                        itemdata : [ "关于 Chishazi.cn", "Chishazi.cn团队", "首选项" ]
+                    }
+
                 },
-                { position: 'left',
-                    width: 150,
-                    resize: true,
-                    body: 'left'
+
+                {
+                    text :"File",
+                    submenu : {
+                        id :"filemenu",
+                        itemdata : [
+
+                                {
+                                    text :"New File",
+                                    helptext :"Ctrl + N"
+                                },
+                                "New Folder",
+                                {
+                                    text :"Open",
+                                    helptext :"Ctrl + O"
+                                },
+                                {
+                                    text :"Open With...",
+                                    submenu : {
+                                        id :"applications",
+                                        itemdata : [ "Application 1",
+                                                "Application 2",
+                                                "Application 3",
+                                                "Application 4" ]
+                                    }
+                                }, {
+                                    text :"Print",
+                                    helptext :"Ctrl + P"
+                                }
+
+                        ]
+                    }
+
                 },
-                { position: 'footer',
-                    height: 30
+
+                {
+                    text :"Edit",
+                    submenu : {
+                        id :"editmenu",
+                        itemdata : [
+
+                        [ {
+                            text :"Undo",
+                            helptext :"Ctrl + Z"
+                        }, {
+                            text :"Redo",
+                            helptext :"Ctrl + Y",
+                            disabled :true
+                        } ],
+
+                        [ {
+                            text :"Cut",
+                            helptext :"Ctrl + X",
+                            disabled :true
+                        }, {
+                            text :"Copy",
+                            helptext :"Ctrl + C",
+                            disabled :true
+                        }, {
+                            text :"Paste",
+                            helptext :"Ctrl + V"
+                        }, {
+                            text :"Delete",
+                            helptext :"Del",
+                            disabled :true
+                        } ],
+
+                        [ {
+                            text :"Select All",
+                            helptext :"Ctrl + A"
+                        } ],
+
+                        [ {
+                            text :"Find",
+                            helptext :"Ctrl + F"
+                        }, {
+                            text :"Find Again",
+                            helptext :"Ctrl + G"
+                        } ]
+
+                        ]
+                    }
+
                 },
-                { position: 'right',
-                    width: 150
-                },
-                { position: 'center'
-                }
-            ]
+
+                "View",
+
+                "Favorites",
+
+                "Tools",
+
+                "Help" ];
+
+        var menu = new YAHOO.widget.MenuBar("header1-nav-menu", {
+            itemData :aItemData,
+            autosubmenudisplay :true,
+            hidedelay :750,
+            lazyload :true,
+            effect : {
+                effect :YAHOO.widget.ContainerEffect.FADE,
+                duration :0.25
+            }
         });
 
-        Cn.Chishazi.Admin.layout.on('render', function() {
-            Cn.Chishazi.Admin.layout.getUnitByPosition('right').collapse();
-            Cn.Chishazi.Admin.layout.getUnitByPosition('footer').collapse();
-        }, Cn.Chishazi.Admin.layout, true);
-
-        Cn.Chishazi.Admin.layout.render();
-
-
+        menu.render(document.getElementById("header1-nav"));
     };
+
+    YAHOO.util.Event.onDOMReady( function() {
+        var layout = new YAHOO.widget.Layout({
+            units : [ {
+                position :'top',
+                height :28,
+                body :'header1',
+                scroll :null,
+                zIndex :2
+            }, {
+                position :'right',
+                header :'Right',
+                width :300,
+                resize :true,
+                footer :'Footer',
+                collapse :true,
+                scroll :true,
+                body :'right1',
+                animate :true,
+                gutter :'5'
+            }, {
+                position :'bottom',
+                height :30,
+                body :'footer1'
+            }, {
+                position :'left',
+                header :'Left',
+                width :200,
+                body :'left1',
+                gutter :'5',
+                scroll :null,
+                zIndex :1
+            }, {
+                position :'center',
+                body :'center1',
+                gutter :'5 0'
+            } ]
+        });
+        layout.on('render', function() {
+            YAHOO.util.Event.onContentReady("header1-nav", initTopMenu);
+            // this.getUnitByPosition('right').collapse();
+                // this.getUnitByPosition('footer').collapse();
+            }, layout, true);
+
+        layout.render();
+    });
+
 })();
