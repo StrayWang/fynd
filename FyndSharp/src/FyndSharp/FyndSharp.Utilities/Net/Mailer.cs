7dd34febc29c64e947693c2a106d745033ab6559
fishtrees <fishtrees@02afeb50-87f6-11dd-aace-ddc5e87cd236>
2011-12-22 10:59:03 +0000

    add mailer class to help send email

---
+++ FyndSharp/src/FyndSharp/FyndSharp.Utilities/Net/Mailer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FyndSharp.Utilities.Net
+{
+    public class Mailer
+    {
+        public string SmtpServerAddress { get; set; }
+        public int SmtpServerPort { get; set; }
+        public string Passport { get; set; }
+        public string Password { get; set; }
+
+        public void SendMail(string from, string to, string subject, string body) { }
+    }
+}
