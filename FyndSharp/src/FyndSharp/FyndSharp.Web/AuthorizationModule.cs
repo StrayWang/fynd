98a5ea89a247451332ac3bd1aca170b6750f5054
fishtrees@gmail.com <fishtrees@gmail.com@02afeb50-87f6-11dd-aace-ddc5e87cd236>
2012-05-20 15:47:56 +0000

    ADD Authorization

---
+++ FyndSharp/src/FyndSharp/FyndSharp.Web/AuthorizationModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace FyndSharp.Web
+{
+    public class AuthorizationModule : IHttpModule
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.PreRequestHandlerExecute += new EventHandler(OnPreRequestHandlerExecute);
+        }
+
+        protected virtual void OnPreRequestHandlerExecute(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
