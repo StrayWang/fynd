4b9cf6030f62c14afb5ad27a3c360f4383e3dd22
fishtrees@gmail.com <fishtrees@gmail.com@02afeb50-87f6-11dd-aace-ddc5e87cd236>
2013-05-27 11:38:39 +0000

    missing file

---
+++ FyndSharp/src/FyndSharp/FyndSharp.Data/MySQLAdoSession.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace FyndSharp.Data
+{
+    class MySQLAdoSession : AdoSession
+    {
+        public MySQLAdoSession(string theConnectionString)
+            : base(theConnectionString)
+        {
+        }
+
+        protected override System.Data.Common.DbConnection CreateDbConnection()
+        {
+            return new MySqlConnection();
+        }
+
+        protected override System.Data.Common.DbCommand CreateDbCommand()
+        {
+            return new MySqlCommand();
+        }
+
+        protected override System.Data.Common.DbParameter CreateDbParameter()
+        {
+            return new MySqlParameter();
+        }
+
+        protected override System.Data.IDataAdapter CreateDataAdapter(System.Data.Common.DbCommand cmd)
+        {
+            return new MySqlDataAdapter((MySqlCommand)cmd);
+        }
+
+        public override string GetLastAutoIncrementValueSql()
+        {
+            return "SELECT LAST_INSERT_ID();";
+        }
+    }
+}
